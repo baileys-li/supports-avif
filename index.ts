b55ae50436c6693ab8d412f4bc175a4337f0f24c
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-05-16 19:30:33 +0300

    Add script

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-// TODO: Start you code here
+export const avifSupport = new Promise<boolean>(resolve => {
+	const image = new Image();
+	console.log("avifSupport");
+	image.addEventListener('error', () => resolve(false));
+	image.addEventListener('load', () => resolve(image.width === 2));
+	image.src =  "data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=";
+}).catch(() => false);
+
+export default avifSupport;
